f2ee3cca95234ffc1e21e36437e553c1e7c0cfef
And3rson-Paiva <and3rsonpaiva@gmail.com>
2020-10-16 12:57:20 -0300

    inclusão meta class em serializers, endereco viewset e rota enderecos

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
-from core.api.viewsets import AtracoesViewSet
-
+from atracoes.api.viewsets import AtracoesViewSet
+from enderecos.api.viewsets import EnderecoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
+router.register(r'enderecos', EnderecoViewSet)
 
 
 urlpatterns = [
